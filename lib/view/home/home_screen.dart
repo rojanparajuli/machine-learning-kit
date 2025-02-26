c45f2459f881977b64244e10a1b1d263b4911ae0
Rojan Parajuli <rojanparajuli13@gmail.com>
2025-02-26 13:32:09 +0545

    language detector

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ml_kit_test/constant/app_color.dart';
 import 'package:ml_kit_test/view/barcode/barcode_scanner_screen.dart';
 import 'package:ml_kit_test/view/digitalInk/digital_ink_recognizer_view.dart';
 import 'package:ml_kit_test/view/document/document_recognizer_screen.dart';
@@ -22,87 +21,36 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<Map<String, dynamic>> itemList = [
-    {
-      'title': 'Text Scanner',
-      'screen': TextScannerScreen(),
-      'icon': Icons.text_fields
-    },
-    {
-      'title': 'Label Scanner',
-      'screen': LabelScannerScreen(),
-      'icon': Icons.label_important_outline
-    },
-    {
-      'title': 'Barcode Scanner',
-      'screen': BarcodeScannerScreen(),
-      'icon': Icons.qr_code
-    },
-    {
-      'title': 'Face Detection',
-      'screen': FaceDetectionScreen(),
-      'icon': Icons.face
-    },
-    {
-      'title': 'Document Scanner',
-      'screen': DocumentScannerScreen(),
-      'icon': Icons.document_scanner
-    },
-    {
-      'title': 'Digital Ink Recognition',
-      'screen': DigitalInkView(),
-      'icon': Icons.edit
-    },
-    {
-      'title': 'Pose Detection',
-      'screen': PoseDetectionScreen(),
-      'icon': Icons.accessibility
-    },
-    {
-      'title': 'subject segmentation',
-      'screen': SubjectSegmentationScreen(),
-      'icon': Icons.data_object
-    },
-    {
-      'title': 'Selfie segmentation',
-      'screen': SelfieSegmentationScreen(),
-      'icon': Icons.person
-    },
+    {'title': 'Text Scanner', 'screen': TextScannerScreen(), 'icon': Icons.text_fields},
+    {'title': 'Label Scanner', 'screen': LabelScannerScreen(), 'icon': Icons.label_important_outline},
+    {'title': 'Barcode Scanner', 'screen': BarcodeScannerScreen(), 'icon': Icons.qr_code},
+    {'title': 'Face Detection', 'screen': FaceDetectionScreen(), 'icon': Icons.face},
+    {'title': 'Document Scanner', 'screen': DocumentScannerScreen(), 'icon': Icons.document_scanner},
+    {'title': 'Digital Ink Recognition', 'screen': DigitalInkView(), 'icon': Icons.edit},
+    {'title': 'Pose Detection', 'screen': PoseDetectionScreen(), 'icon': Icons.accessibility},
+    {'title': 'Subject Segmentation', 'screen': SubjectSegmentationScreen(), 'icon': Icons.data_object},
+    {'title': 'Selfie Segmentation', 'screen': SelfieSegmentationScreen(), 'icon': Icons.person},
     {'title': 'Smart Reply', 'screen': SmartReplyView(), 'icon': Icons.reply},
-    {
-      'title': 'Language Detector',
-      'screen': LanguageDetectorScreen(),
-      'icon': Icons.language
-    },
-    {
-      'title': 'Language Translator',
-      'screen': LanguageTranslatorScreen(),
-      'icon': Icons.translate
-    },
+    {'title': 'Language Detector', 'screen': LanguageDetectorScreen(), 'icon': Icons.language},
+    {'title': 'Language Translator', 'screen': LanguageTranslatorScreen(), 'icon': Icons.translate},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.primaryColor,
       appBar: AppBar(
-        backgroundColor: AppColor.primaryColor,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         title: const Text(
           "Machine Learning Kit",
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
       ),
+      extendBodyBehindAppBar: true,
       body: Stack(
         children: [
-          Opacity(
-            opacity: 0.1,
-            child: Image.asset(
-              'assets/logo-vertical.png',
-              width: double.infinity,
-              height: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
+          _buildGradientBackground(),
           Padding(
             padding: const EdgeInsets.all(12.0),
             child: GridView.builder(
@@ -110,51 +58,65 @@ class _HomeScreenState extends State<HomeScreen> {
                 crossAxisCount: 2,
                 crossAxisSpacing: 12,
                 mainAxisSpacing: 12,
-                childAspectRatio: 1.7,
+                childAspectRatio: 1.3,
               ),
               itemCount: itemList.length,
               itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => itemList[index]['screen'],
-                      ),
-                    );
-                  },
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    elevation: 5,
-                    color: Colors.white,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          itemList[index]['icon'],
-                          size: 50,
-                          color: Colors.black,
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          itemList[index]['title'],
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+                return _buildAnimatedCard(context, index);
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildGradientBackground() {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF0F2027), Color(0xFF203A43), Color(0xFF2C5364)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedCard(BuildContext context, int index) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => itemList[index]['screen']),
+        );
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        decoration: BoxDecoration(
+          color: Colors.white.withValues(alpha: 0.15),
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(color: Colors.white.withValues(alpha: 0.3), width: 1),
+          boxShadow: [
+            BoxShadow(color: Colors.black26, blurRadius: 10, offset: Offset(3, 3)),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              itemList[index]['icon'],
+              size: 50,
+              color: Colors.white,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              itemList[index]['title'],
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
